88f5a4a4663da2f07c0578a57cb6f9969fcc5e2e
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-08-11 20:05:08 +0700

    initial pokemon detail with evolutions

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,10 +1,14 @@
-import { PokemonClient } from 'pokenode-ts';
+import { Pokemon, PokemonClient } from 'pokenode-ts';
 
 const api = new PokemonClient({
   cacheOptions: {
     maxAge: 1000 * 60 * 60 * 24 // 1 day
   }
 });
+
+function getPokemonImageUrl(pokemon: Pokemon): string {
+  return pokemon.sprites.other.dream_world.front_default ?? (pokemon.sprites.other.home.front_default as string);
+}
 
 export async function getAllPokemons() {
   const listPokemons = await api.listPokemons(0, 10);
@@ -19,7 +23,7 @@ export async function getAllPokemons() {
   return pokemonsWithDetail.map(p => ({
     name: p.name,
     detail: {
-      imageUrl: p.detail.sprites.other.dream_world.front_default ?? p.detail.sprites.other.home.front_default,
+      imageUrl: getPokemonImageUrl(p.detail),
       types: p.detail.types.map(t => t.type.name)
     }
   }));
@@ -27,4 +31,24 @@ export async function getAllPokemons() {
 
 export async function getPokemonDetail(name: string) {
   return await Promise.all([api.getPokemonSpeciesByName(name), api.getPokemonByName(name)]);
+}
+
+export async function getEvoDetails(evolutionChainUrl: string) {
+  const evoChainResult = await fetch(evolutionChainUrl);
+  const evoChainJson = await evoChainResult.json();
+  let evoData = evoChainJson.chain;
+  const evoChains: string[] = [];
+
+  while (evoData?.hasOwnProperty('evolves_to')) {
+    evoChains.push(evoData.species.name);
+
+    evoData = evoData.evolves_to[0];
+  }
+
+  const pokemons = await Promise.all(evoChains.map(name => api.getPokemonByName(name)));
+
+  return pokemons.map(poke => ({
+    name: poke.name,
+    imageUrl: getPokemonImageUrl(poke)
+  }));
 }
